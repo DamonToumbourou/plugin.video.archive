71825640f59265275bea85663169fd894b979c4c
Damon Toumbourou <dtoumbourou@gmail.com>
2016-01-06 16:34:08 +1100

    play content added

--- addon.py
+++ addon.py
@@ -1,9 +1,8 @@
 from xbmcswift2 import Plugin, xbmcgui
-from resources.lib import topdocos
+from resources.lib import archive
 
-
+SITE_URL = 'https://archive.org'
 PLUGIN_URL = 'plugin://plugin.video.youtube/?action=play_video&videoid='
-SITE_URL = 'http://topdocumentaryfilms.com/'
 
 plugin = Plugin()
 
@@ -13,140 +12,61 @@ def main_menu():
 
     items = [
         {
-            'label': plugin.get_string(30001),
-            'path': plugin.url_for('recent'),
-        },
-        {
-            'label': plugin.get_string(30002),
-            'path': plugin.url_for('highest_rated'),
-        },
-        {
-            'label': plugin.get_string(30003),
-            'path': plugin.url_for('most_voted'),
-        },
-        {
-            'label': plugin.get_string(30004),
-            'path': plugin.url_for('most_shared'),
-        },
-        { 
-            'label': plugin.get_string(30005),
-            'path': plugin.url_for('categorys'),
+            'label': plugin.get_string(30000),
+            'path': plugin.url_for('views'),
         }
     ]
     
     return items
 
 
-@plugin.route('/recent/')
-def recent():
+@plugin.route('/views/')
+def views():
+    
+    view_url = 'https://archive.org/details/movies?&sort=-downloads&page=1'
     
     items = []
 
-    content = topdocos.get_section(SITE_URL, 'recently added')
+    content = archive.get_views(view_url)
 
     for i in content:
         items.append({
             'label': i['label'],
-            'path': plugin.url_for('play_content', url=i['path']),
+            'path': plugin.url_for('view_category', url=SITE_URL + i['path']),
             'thumbnail': i['thumbnail'],
         })
 
     return items
 
 
-@plugin.route('/highest_rated/')
-def highest_rated():
+@plugin.route('/views/<url>/')
+def view_category(url):
 
     items = []
 
-    content = topdocos.get_section(SITE_URL, 'highest rated')
+    content = archive.get_category(url)
 
     for i in content:
         items.append({
             'label': i['label'],
-            'path': plugin.url_for('play_content', url=i['path']),
-        })
-
-    return items
-
-
-@plugin.route('/most_voted/')
-def most_voted():
-
-    items = []
-
-    content = topdocos.get_section(SITE_URL, 'most voted')
-
-    for i in content:
-        items.append({
-            'label': i['label'],
-            'path': plugin.url_for('play_content', url=i['path']),
-        })
-
-    return items
-
-
-@plugin.route('/most_shared/')
-def most_shared():
-
-    items = []
-
-    content = topdocos.get_section(SITE_URL, 'most shared')
-
-    for i in content:
-        items.append({
-            'label': i['label'],
-            'path': plugin.url_for('play_content', url=i['path']),
-        })
-
-    return items
-
-
-@plugin.route('/categorys/')
-def categorys():
-    url = 'http://topdocumentaryfilms.com/all/'
-
-    items = []
-
-    content = topdocos.get_categorys(url)
-   
-    for i in content:
-        items.append({
-            'label': i['label'],
-            'path': plugin.url_for('get_categorys', url=i['path']),
-        })
-
-    return items
-
-
-@plugin.route('/categorys/<url>')
-def get_categorys(url):
-
-    items = []
-
-    content = topdocos.get_categorys_content(url)
-
-    for i in content:
-        items.append({
-            'label': i['label'],
-            'path': plugin.url_for('play_content', url=i['path']),
+            'path': plugin.url_for('play_content', url=SITE_URL + i['path']),
             'thumbnail': i['thumbnail'],
         })
 
     return items
 
 
-@plugin.route('/doco_categorys/get_categorys/<url>/')
+@plugin.route('/views/view_category/play_content/<url>')
 def play_content(url):
 
     items = []
 
-    content = topdocos.play_categorys(url)
+    content = archive.play_content(url)
 
     for i in content:
         items.append({
             'label': i['label'],
-            'path': PLUGIN_URL + i['path'],
+            'path': SITE_URL + i['path'],
             'is_playable': True,
         })
 
--- resources/lib/archive.py
+++ resources/lib/archive.py
@@ -14,6 +14,8 @@ def get_views(url):
     soup = get_soup(url)
     content = soup.find_all('div', {'class': 'item-ia collection-ia'})
 
+    output = []
+
     for i in content:
         data = i.find('a')
 
@@ -34,4 +36,69 @@ def get_views(url):
         except AttributeError:
             continue
 
-#get_views('https://archive.org/details/movies?sort=-downloads')
+        items = {
+            'label': label,
+            'path': path,
+            'thumbnail': thumb,
+        }
+        
+        output.append(items)
+
+    return output
+#get_views('https://archive.org/details/movies?&sort=-downloads&page=1')
+
+
+def get_category(url):
+    soup = get_soup(url)
+    content = soup.find_all('div', {'class': 'item-ttl'})
+
+    output = []
+
+    for i in content:
+        label = i.get_text().strip()
+
+        path = i.find('a').get('href')
+
+        try:
+            thumb = str(i.img)
+            thumb = re.search('/services(.*)(?=" s)', thumb).group(0)
+
+        except AttributeError:
+            continue
+
+        items = { 
+            'label': label,
+            'path': path,
+            'thumbnail': thumb,
+        }
+
+        output.append(items)
+
+    return output
+get_category('https://archive.org/details/opensource_movies')
+
+
+def play_content(url):
+    DOWNLOAD_CHOICE = 'MPEG4'
+    VIDEO_LABEL = 'play video'
+
+    soup = get_soup(url)
+    content = soup.find_all('div', {'class': 'format-group'})
+   
+    output = []
+
+    for i in content:
+        download_type = i.get_text()
+        if DOWNLOAD_CHOICE in download_type:
+            path = i.find('a').get('href')
+            label = 'Play Video'
+
+            items = { 
+                'path': path,
+                'label': VIDEO_LABEL,
+            }
+
+            output.append(items)
+
+    return output
+#play_content('https://archive.org/details/20151212-08277s')
